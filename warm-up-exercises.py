4ec05180c2ffd4ed609057eca386bd44fb463877
Ignasia Hanny <52771825+hannyign@users.noreply.github.com>
2022-11-17 16:13:05 +0800

    Add files via upload

---
+++ warm-up-exercises.py
@@ -0,0 +1,31 @@
+""" ### Exercise 1: print “Hello World” 
+# Hint 1: press the "PLAY" button at the top, or hit F5 to test your code! """
+# Your Code Here:
+
+
+
+
+""" ### Exercise 2: Create a function to sum 2 numbers (let’s say a and b ). Feel free to experiment!
+# Hint 1: all functions start with def <name of function>(variable):
+# Hint 2: remember to define what your function returns using 'return'!
+# Hint 3: don't forget to print! """
+# Your Code Here:
+
+
+
+
+""" ### Exercise 3:  Create a function to check the number is odd or even
+# Hint 1: Check the in-built function documentation! """
+# Your Code Here:
+
+
+
+
+""" ### Exercise 4: Import a module called matplotlib.pyplot and rename it as plt"""
+# Your Code Here:
+
+
+
+
+""" ### Exercise 5:  Use a module called randint to generate 10 random number from 1-100"""
+# Hint 1: Check the in-built function documentation! """
